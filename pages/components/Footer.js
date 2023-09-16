36ec0134fe1f690ef9ccbf25f4871064c0970d70
makiharu <kitakazetoshirokuma@gmail.com>
2023-09-16 19:13:30 +0900

    init

---
+++ pages/components/Footer.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export function footer() {
+  return (
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+        </a>
+      </footer>
+  );
+}
