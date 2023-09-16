f8d4fc725f8ad201afe9efa7f72ebc631bb4c981
makiharu <kitakazetoshirokuma@gmail.com>
2023-09-16 19:48:32 +0900

    💡 テスト

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          テストテスト　Powered by{' '}
+          テスト　Powered by{' '}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
